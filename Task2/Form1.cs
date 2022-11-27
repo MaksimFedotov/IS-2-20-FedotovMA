674adc322ad657456e4608f3b435fbea2279c277
MaksimFedotov <f3d0t0vmax@yandex.ru>
2022-11-27 12:10:11 +0500

    Задание 2

--- Task2/Form1.cs
+++ Task2/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace Task2
 {
     public partial class Form1 : Form
@@ -16,5 +19,54 @@ namespace Task2
         {
             InitializeComponent();
         }
+
+        public class DBconnection
+        {
+            protected string _connectString;
+           
+
+            public DBconnection(string connectString)
+            {
+                this._connectString = connectString;
+
+            }
+
+            public string GetConnectionString()
+            {
+                return this._connectString;
+            }
+        }
+
+        DBconnection db = new DBconnection("server=chuc.caseum.ru;port=33333;user=uchebka;" +
+                "database=uchebka;password=uchebka;");
+
+        MySqlConnection conn;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new MySqlConnection(db.GetConnectionString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                MessageBox.Show("Соединение работает отлично!");
+
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка при открытии соединения!");
+
+            }
+            finally
+            {
+                MessageBox.Show("Хорошего дня в любом случае!");
+                conn.Close();
+            }
+        }
+
+       
     }
 }
